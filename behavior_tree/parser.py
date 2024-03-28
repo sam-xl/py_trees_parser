ca2ed6ef52ee3c4dbad6ed3adc4f7b21e411ebd6
Erich L Foster <erichlf@gmail.com>
2024-03-28 11:27:52 +0100

    Remove unneeded include and function call

--- behavior_tree/parser.py
+++ behavior_tree/parser.py
@@ -1,5 +1,5 @@
 import importlib
-from xml.etree import ElementInclude, ElementTree
+from xml.etree import ElementTree
 
 
 class BTParseError(Exception):
@@ -146,7 +146,5 @@ class BTParser:
 
     def parse(self):
         xml = ElementTree.parse(self.file)
-
-        ElementInclude.include(xml)
 
         return self._build_tree(xml.getroot())
