cdc93fb58f0afb4803763c048101572bf4eba162
syrinezbidi <s.zbidi@tudelft.nl>
2024-08-12 13:33:41 +0000

    correct imports, docstrings and missing method

--- behavior_tree/__init__.py
+++ behavior_tree/__init__.py
@@ -1,5 +1,13 @@
+from .blackboard import read_from_blackboard, write_to_blackboard
 from .data import Blackboards, State
 from .parser import BTParser
 from .robot import Robot
 
-__all__ = ("Blackboards", "BTParser", "Robot", "State")
+__all__ = (
+    "Blackboards",
+    "BTParser",
+    "Robot",
+    "State",
+    "read_from_blackboard",
+    "write_to_blackboard",
+)
