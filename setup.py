10e612431c9d24fce2245c9d2e9411b97eace70d
Erich L Foster <erichlf@gmail.com>
2024-10-11 09:14:49 +0000

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = "behavior_tree"
 
 setup(
     name=package_name,
-    version="0.4.2",
+    version="0.4.3",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", [os.path.join("resource", package_name)]),
