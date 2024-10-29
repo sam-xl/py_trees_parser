bca81260f60621f3a412b0269027f62e81d6e6cb
Erich L Foster <erichlf@gmail.com>
2024-10-29 12:39:19 +0000

    Don't consume waypoints or trajectory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = "behavior_tree"
 
 setup(
     name=package_name,
-    version="0.5.4",
+    version="0.5.5",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", [os.path.join("resource", package_name)]),
