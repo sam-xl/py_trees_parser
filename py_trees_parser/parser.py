f68c72ca28e8d80abe696b41ab0f388ba29ae266
Erich L Foster <erichlf@gmail.com>
2025-09-08 15:20:50 +0000

    Add log message message for special characters

--- py_trees_parser/parser.py
+++ py_trees_parser/parser.py
@@ -502,11 +502,17 @@ class BTParser:
         elif on_selected is not None:
             self.logger.debug("Found SuccessOnSelected in parameters")
 
+            def replace_special(char: str):
+                if char.isalnum():
+                    return char
+
+                self.logger.warning(f"Found special {char} in behavior name, replacing with '_'")
+                return "_"
+
             selection = [
                 child
                 for child in children
-                if "".join(map(lambda char: char if char.isalnum() else "_", child.name))
-                in on_selected
+                if "".join(map(replace_special, child.name)) in on_selected
             ]
             if len(selection) == 0:
                 raise ValueError("List of children for SuccessOnSelected is empty")
